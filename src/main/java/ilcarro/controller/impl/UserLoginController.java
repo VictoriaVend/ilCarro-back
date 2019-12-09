97fbfee693dc897b512eafc61920a8e2cf1c9eaa
Arthur Khachaturian <58505537+ArthurKhachaturian@users.noreply.github.com>
2019-12-09 10:35:22 +0200

    implements controller userLogin

--- src/main/java/ilcarro/controller/impl/UserLoginController.java
+++ src/main/java/ilcarro/controller/impl/UserLoginController.java
@@ -11,15 +11,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RequestMapping(value = "/user/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 @Api(tags = {"UserLogin"})
 public class UserLoginController {
         @GetMapping(value = "/")
         @ApiOperation(value = "login", response = UserFullView.class, tags = {"login"})
-        @ApiResponses(value = {
-                @ApiResponse(code = 200, message = "OK"),
-                @ApiResponse(code = 401, message = "Unauthorized")
+        @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 401, message = "Unauthorized")
         })
         public ResponseEntity<UserFullView> getCurrentCapacityById (@RequestBody final UserFullView userView) {
             throw new NotImplementedException();
